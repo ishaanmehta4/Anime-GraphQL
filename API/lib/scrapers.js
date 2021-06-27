fe5d05749c3ccfbed72e6625663afca6cc1fbced
Ishaan Mehta <ishaanmehta4@gmail.com>
2021-06-28 00:10:33 +0530

    Added characterNames to anime scraper.

--- API/lib/scrapers.js
+++ API/lib/scrapers.js
@@ -1,24 +1,25 @@
 const axios = require('axios')
 const jsdom = require("jsdom");
 
-async function getCharacterData(characterName) {
+exports.getCharacterData = getCharacterData = async function (characterName) {
+    if(!characterName) return [null, 'Character name is required']
     let characterData = {}
-    let searchUrl = 'https://www.animecharactersdatabase.com/searchall.php?searchin=c&sq=' + characterName.toLowerCase().replace(' ','+')
-    console.log(`fetching search results for ${characterName}...`)
+
+    let searchUrl = 'https://www.animecharactersdatabase.com/searchall.php?searchin=c&sq=' + characterName.toLowerCase().replace(' ', '+')
     let searchRes = await axios.get(searchUrl)
     let dom = (new jsdom.JSDOM(searchRes.data))
     dom = dom.window.document;
-    if(!dom.querySelector('#tile1 > ul > li:nth-child(2) > div.tile3top > a')) {
-        console.log('No results for ' + characterName)
-        return {error: 'not found'}
+
+    if (!dom.querySelector('#tile1 > ul > li:nth-child(2) > div.tile3top > a')) {
+        return [null, `No results for ${characterName}`]
     }
+
     let profileUrl = 'https://www.animecharactersdatabase.com/' + dom.querySelector('#tile1 > ul > li:nth-child(2) > div.tile3top > a').href;
-    console.log('fetching profile...')
     let profileRes = await axios.get(profileUrl)
     dom = (new jsdom.JSDOM(profileRes.data))
     dom = dom.window.document;
-    // console.log('Info: ', dom.querySelector('#characterzone > table > tbody > tr > td:nth-child(2) > p').textContent)
 
+    characterData.name = dom.querySelector('#main1 > h1 > a.fgw').textContent.replace(/\r?\n|\r/g, " ").trim()
     characterData.bio = dom.querySelector('#characterzone > table > tbody > tr > td:nth-child(2) > p').textContent.replace(/\r?\n|\r/g, " ").trim()
     characterData.profileUrl = profileUrl
     characterData.image = 'https://ami.animecharactersdatabase.com/uploads/chars/' + dom.querySelector('#sidephoto > img').src.split('/').pop().replace('.jpg', '.png')
@@ -27,57 +28,48 @@ async function getCharacterData(characterName) {
     dom.querySelectorAll('#sidephoto > table:nth-child(4) > tbody > tr').forEach(row => {
         let tempDom = new jsdom.JSDOM(row.innerHTML.replace('th>', 'p>').replace('td>', 'p>'))
         tempDom = tempDom.window.document
-        // console.log(tempDom.querySelectorAll('p')[1].textContent)
         let paragraphElements = tempDom.querySelectorAll('p')
-        if(paragraphElements.length == 2 && paragraphElements[0].textContent.replace(/\s/g,'').length <= 15) {
-            characterData[paragraphElements[0].textContent.replace(/\s/g,'').toLowerCase()] =paragraphElements[1].textContent.trim()
-            // replace(/\r?\n|\t|\r/g, "")
+        if (paragraphElements.length == 2 && paragraphElements[0].textContent.replace(/\s/g, '').length <= 15) {
+            characterData[paragraphElements[0].textContent.replace(/\s/g, '').toLowerCase()] = paragraphElements[1].textContent.trim()
         }
     })
 
-    characterData.voicedby = characterData.voicedby.split(',').filter(name=> name.trim() !='')
-    characterData.tags = characterData.tags.split(',').map(tag=>tag.trim())
+    characterData.voicedby = characterData.voicedby.split(',').filter(name => name.trim() != '')
+    characterData.tags = characterData.tags.split(',').map(tag => tag.trim())
 
-    console.log(characterData)
-    return characterData
+    return [characterData, null]
 }
 
-async function getAnimeData(animeName) {
+exports.getAnimeData = getAnimeData = async function (animeName) {
+    if(!animeName) return [null, 'Anime name is required']
     let animeData = {}
-    let searchUrl = 'https://www.animecharactersdatabase.com/searchall.php?in=titles&sq=' + animeName.toLowerCase().replace(' ','+')
-    console.log(`fetching search results for ${animeName}...`)
+
+    let searchUrl = 'https://www.animecharactersdatabase.com/searchall.php?in=titles&sq=' + animeName.toLowerCase().replace(' ', '+')
     let searchRes = await axios.get(searchUrl)
     let dom = (new jsdom.JSDOM(searchRes.data))
     dom = dom.window.document;
-    if(!dom.querySelector('#tile1 > ul > li:nth-child(2) > div.tile3top > a')) {
-        console.log('No results for ' + animeName)
-        return {error: 'not found'}
+
+    if (!dom.querySelector('#tile1 > ul > li:nth-child(2) > div.tile3top > a')) {
+        return [null, `No results for ${animeName}`]
     }
     let profileUrl = 'https://www.animecharactersdatabase.com/' + dom.querySelector('#tile1 > ul > li:nth-child(2) > div.tile3top > a').href;
-    console.log('fetching profile...')
     let profileRes = await axios.get(profileUrl)
     dom = (new jsdom.JSDOM(profileRes.data))
     dom = dom.window.document;
 
+    animeData.name = dom.querySelector('#main1 > h1').textContent.replace(/\r?\n|\r/g, " ").trim().split(' | ')[0]
     animeData.plot = dom.querySelector('#mainframe2 > #besttable > table > tbody > tr > td').textContent.replace(/\r?\n|\r/g, " ").trim()
     animeData.profileUrl = profileUrl
     animeData.image = dom.querySelector('#bt > tbody > tr:nth-child(1) > th > a > img').src
-    animeData.characters = []
-    dom.querySelectorAll('#tile1 > ul > li').forEach(card=> {
-        // let tempDom = new jsdom.JSDOM(card.innerHTML.replace('a>', 'p>'))
+    animeData.characterNames = []
+
+    dom.querySelectorAll('#tile1 > ul > li').forEach(card => {
         let tempDom = new jsdom.JSDOM(card.innerHTML)
         tempDom = tempDom.window.document
-        // console.log(tempDom.querySelector('a').textContent)
-        if(tempDom.querySelector('a'))
-        animeData.characters.push({
-            characterName: tempDom.querySelector('a').textContent.trim(),
-            profileUrl: 'https://www.animecharactersdatabase.com/' + tempDom.querySelector('a').href
-        })
+
+        if (tempDom.querySelector('a'))
+            animeData.characterNames.push(tempDom.querySelector('a').textContent.trim())
     })
 
-    console.log(animeData)
-    return animeData
+    return [animeData, null]
 }
-
-getAnimeData('My hero academia')
-// getCharacterData('Ochako')
