382bf72eba52dd8db3f11a429604ced53d7d07ac
Ishaan Mehta <ishaanmehta4@gmail.com>
2021-07-04 00:48:35 +0530

    Added build for frontend, updated README.

--- API/index.js
+++ API/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const morgan = require('morgan');
+const path = require('path');
 
 const graphqlHTTP = require('./graphql');
 const restRouter = require('./rest')
@@ -12,12 +13,18 @@ app.use(cors());
 app.use(morgan('dev'));
 
 // ---------- Routes ----------
-app.get('/', (req, res) => {
-  res.send({ status: 'success', message: 'Server running. Send queries to /graphql endpoint.' });
-});
+// app.get('/', (req, res) => {
+//   res.send({ status: 'success', message: 'Server running. Send queries to /graphql endpoint.' });
+// });
 
 app.use('/graphql', graphqlHTTP);
 app.use('/rest', restRouter)
+
+//---------- Root Route for frontend ----------
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+app.use(express.static(path.join(__dirname, 'build')))
 
 // ---------- Middlewares ----------
 app.listen(process.env.PORT || 5000, () => {
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Anime-GraphQL
-A GraphQL api that provides data about anime and anime characters based on search strings.
+A GraphQL API that provides data about anime and anime characters, build using express-graphql for backend, and apollo-client with ReactJS for frontend.
+
+The data is fetched at runtime using web-scraping from [www.animecharactersdatabase.com](https://www.animecharactersdatabase.com/).
+
+REST routes are also available :)
+
+### Try it out!!
+ - [Open AnimeGQL](https://anime-gql.herokuapp.com/)
